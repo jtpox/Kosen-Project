452a54363a8aec46af25f6804174f9be0f54e28f
jtPox <me@jtpox.com>
2018-12-04 09:31:51 +0800

    Calling websocket.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import Express from 'express';
 
+import Ws from 'ws';
+
 import dotenv from 'dotenv';
 
 import Kosen from './app/kosen';
 
+import Websocket from './app/websocket';
+
 dotenv.config();
 
-const kosen = new Kosen(Express);
+const kosen = new Kosen(Express); // Start Web Server.
+
+const websocket = new Websocket(Ws); // Start Web Socket.
